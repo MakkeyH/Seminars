905f0feb011c1637039074735565bc2681b26c29
MakarovaElena <lena-chester@mai.ru>
2023-07-26 22:59:53 +0300

    The code for Task007 has been made

---
+++ Task007/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает\
+// трехзначное число и на выходе показывает последнюю цифру 
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите целое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number *= -1;
+}
+if (number >= 100 && number <= 999)
+{
+    int result = number % 10;
+    Console.WriteLine($"Последняя цифра числа: {result}");
+}
+else
+{
+    Console.WriteLine($"{number} - НЕ трёхзначное число");
+}
