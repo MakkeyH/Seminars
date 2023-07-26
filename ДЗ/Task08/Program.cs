a7ab1aa28efe756eef1e8207b48e536ce5707fda
MakarovaElena <lena-chester@mai.ru>
2023-07-27 02:27:45 +0300

    Изменения в Task08

---
+++ ДЗ/Task08/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.Write($"Число{number} нам не подходит");
+}
+else
+{
+    int count = number;
+    if (count % 2 == 0)
+    {
+        // Console.Write($"{count} - Чётное число");
+        count = count - 0;
+    }
+    else if (count % 2 != 0)
+    {
+        // Console.Write($"{count} - Нечётное число");
+        count = count - 1;
+    }
+}
+
+
