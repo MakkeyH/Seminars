0e3120160dec494b0492b6f968092fea71d4c46e
MakarovaElena <lena-chester@mai.ru>
2023-07-27 11:40:36 +0300

    Написан код к задаче Task04

--- ДЗ/Task04/Program.cs
+++ ДЗ/Task04/Program.cs
@@ -4,12 +4,11 @@
 // 22 3 9 -> 22
 
 Console.WriteLine("Введите три целых числа");
-int number1 = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
-int max = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > max) max = number1;
 if (number2 > max) max = number2;
 if (number3 > max) max = number3;
-Console.Write("max= ");
+
+Console.Write($"{max} - максимальное число");
