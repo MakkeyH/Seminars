17d976de9cee20e3d02619d83ebdcf8f08d9ab19
MakarovaElena <lena-chester@mai.ru>
2023-08-24 17:36:54 +0300

    Существует ли треугольник? Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник со сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+bool Triangle(int s1, int s2, int s3)
+{
+    if (s1 < s2 + s3 && s2 < s1 + s3 && s3 < s1 + s2) return true;
+    else return false;
+}
+
+Console.WriteLine("Ввведите три числа: ");
+int side1 = Convert.ToInt32(Console.ReadLine());
+int side2 = Convert.ToInt32(Console.ReadLine());
+int side3 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Triangle(side1, side2, side3);
+Console.WriteLine(result ? "Треугольник существует" : "Треугольник не существует");
+
+
+
+
+
+
