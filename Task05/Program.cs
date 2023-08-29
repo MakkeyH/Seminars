74f67619eec76768ea3407eb266d5e4cc0b33414
MakarovaElena <lena-chester@mai.ru>
2023-08-29 09:26:43 +0300

    Программа вычисления значения функции возведения числа в квадртат Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 5. Напишите программу вычисления значения функции возведения
+// числа в квадрат.
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+OurPower(number); // вызов метода
+OurPower(7);
+OurPower(number + number);
+
+void OurPower(int currentNumber) // метод , где скобки - тело метода, внутри скобок - 
+// выполнение того или иного действия
+{
+    Console.WriteLine(currentNumber * currentNumber); // currentNumber - встроенная переменная,т.е. 
+// параметр, принадлежащий переменной number 
+}
+
+
+// Console.WriteLine(Math.Pow(number, 2)); // Math.Pow - функция или метод, 
+// позволяющий возводить число в ту или иную степень
