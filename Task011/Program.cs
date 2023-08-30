e66b5e0c87d891b3612172deddf07a06ea817bfc
MakarovaElena <lena-chester@mai.ru>
2023-08-30 14:13:25 +0300

    Конечное число из трехзначного рандомного числа без второй цифры Task011

--- Task011/Program.cs
+++ Task011/Program.cs
@@ -5,13 +5,33 @@
 // 782 -> 72
 // 918 -> 98
 
-int number = new Random().Next(100, 1000);
+// Random rand = new Random();
+// int number = rand.Next(100, 1000);
+// Console.WriteLine($"Cлучайное число - {number}");
+
+// int result = Finish(number);
+// Console.WriteLine($"Конечное число - {result}");
+
+// int Finish(int num)
+// {
+//     int firstDigit = num / 100;
+//     int thirdDigit = num % 10;
+//     return firstDigit * 10 + thirdDigit;
+// }
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
 Console.WriteLine($"Cлучайное число - {number}");
-int result = Finish(number);
-Console.WriteLine($"Конечное число - {result}");
-int Finish(int num)
+
+void FinishNumber(int num)
 {
     int firstDigit = num / 100;
     int thirdDigit = num % 10;
-    return firstDigit * 10 + thirdDigit;
+    int result = firstDigit * 10 + thirdDigit;
+    Console.WriteLine($"Конечное число - {result}");
 }
+
+FinishNumber(number);
+
+
+
