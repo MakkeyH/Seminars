43674408c96a281f5e73e6396ebcff9cff174025
MakarovaElena <lena-chester@mai.ru>
2023-08-31 13:02:03 +0300

    Конечное число из трехзначного рандомного числа без второй цифры Task011

--- Task011/Program.cs
+++ Task011/Program.cs
@@ -1,11 +1,10 @@
 ﻿// 11. Напишите программу, которая выводит случайное
-// трёхзначное число и удаляет вторую цифру этого
-// числа.
+// трёхзначное число и удаляет вторую цифру этого числа.
 // 456 -> 46
 // 782 -> 72
 // 918 -> 98
 
-// Random rand = new Random();
+// Random rand = new Random();     - вариант решения через метод int - return
 // int number = rand.Next(100, 1000);
 // Console.WriteLine($"Cлучайное число - {number}");
 
@@ -19,19 +18,30 @@
 //     return firstDigit * 10 + thirdDigit;
 // }
 
-Random rand = new Random();
-int number = rand.Next(100, 1000);
-Console.WriteLine($"Cлучайное число - {number}");
+// Random rand = new Random();   - вариант решения через метод void
+// int number = rand.Next(100, 1000);
+// Console.WriteLine($"Cлучайное число - {number}");
 
-void FinishNumber(int num)
+// void FinishNumber(int num)
+// {
+//     int firstDigit = num / 100;
+//     int thirdDigit = num % 10;
+//     int result = firstDigit * 10 + thirdDigit;
+//     Console.WriteLine($"Конечное число - {result}");
+// }
+
+// FinishNumber(number);
+
+Console.WriteLine("Введите трёхзначное число: "); // - вариант решения через строку
+Random rand = new Random();
+string number = Console.ReadLine();
+
+void FinishNumber(string num)
 {
-    int firstDigit = num / 100;
-    int thirdDigit = num % 10;
-    int result = firstDigit * 10 + thirdDigit;
-    Console.WriteLine($"Конечное число - {result}");
+    Console.Write(num[0]);
+    Console.Write(num[2]);
 }
 
 FinishNumber(number);
-
 
 
