465f07ec1d88fb1b0fe9aa3bf62531a5cd308408
MakarovaElena <lena-chester@mai.ru>
2023-09-01 20:35:43 +0300

    Цифры, обозначающие дни недели и проверка выходного дня Task15

--- ДЗ/Task15/Program.cs
+++ ДЗ/Task15/Program.cs
@@ -5,9 +5,17 @@
 // 1 -> нет
 
 Console.WriteLine("Введите целое число");
-int a = Convert.ToInt32(Console.ReadLine());
-int weekday = Weekday(a);
-if(0 < a && a <=7)
+int day = Convert.ToInt32(Console.ReadLine());
+void Weekday(int d)
 {
-    
+    if (d == 1) Console.Write($"{d} - Понедельник");
+    else if (d == 2) Console.Write($"{d} -Вторник");
+    else if (d == 3) Console.Write($"{d} - Среда");
+    else if (d == 4) Console.Write($"{d} - Четверг");
+    else if (d == 5) Console.Write($"{d} - Пятница");
+    else if (d == 6) Console.Write($"{d} - Ура!Суббота!Выходной");
+    else if (d == 7) Console.Write($"{d} - Ура!Воскресенье!Выходной");
+    else Console.WriteLine("Такого дня недели не существует");
 }
+
+Weekday(day);
