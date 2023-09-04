08520466df3cdbc6fffa52da3d22f4d422ba33b4
MakarovaElena <lena-chester@mai.ru>
2023-09-04 14:40:28 +0300

    Третья цифра любого числа или программа,сообщающая,что третьей цифры нет Task13

--- ДЗ/Task13/Program.cs
+++ ДЗ/Task13/Program.cs
@@ -4,25 +4,49 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введите целое число");
+// Console.WriteLine("Введите целое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(-100 < number && number < 100)
+// {
+//     Console.WriteLine($"У числа {number} нет третьей цифры");
+// }
+// else
+// {
+//     int result = ThirdRank(number);
+//     int ThirdRank(int number)
+//     {
+//     while(number > 999 || -999 > number)
+//     {
+//         number /=10;
+//     }
+//     return number % 10;
+//     }
+//     Console.WriteLine($"Третья цифра числа {number} - {result}"); 
+// }
+
+Console.WriteLine("Введите число: ");
+
 int number = Convert.ToInt32(Console.ReadLine());
-if(-100 < number && number < 100)
-{
-    Console.WriteLine($"У числа {number} нет третьей цифры");
-}
-else
-{
-    int result = ThirdRank(number);
-    int ThirdRank(int number)
-    {
-    while(number > 999 || -999 > number)
-    {
-        number /=10;
-    }
-    return number % 10;
-    }
-    Console.WriteLine($"Третья цифра числа {number} - {result}"); 
-}
 
 
- 
+void ThirdNum(int num)
+{
+    // string num = Console.ReadLine();
+    // // string num = "2345";
+
+    // if (num.Length > 2) Console.WriteLine(num[2]); // третий символ строки - второй элемент массива;
+    // else Console.WriteLine("У числа нет третьей цифры");
+
+    while (num / 1000 > 0)
+    {
+        num /= 10;
+        Console.WriteLine(num);
+    }
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(number);
+
+
+
+
