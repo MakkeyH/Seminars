80775f26807582479906690e7124d1af093677c4
MakarovaElena <lena-chester@mai.ru>
2023-09-05 22:07:29 +0300

    Таблица квадратов чисел от 1 до N Task022

--- Task022/Program.cs
+++ Task022/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Задача 22: Напишите программу, которая
 // принимает на вход число (N) и выдаёт таблицу
 // квадратов чисел от 1 до N.
+// 5-> 1,4,9,16,25
+// 2-> 1,4
 
 Console.WriteLine("Введите целое натуральное число:");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -10,12 +12,14 @@ if (n < 1)
     Console.WriteLine("Некорректные данные");
     return;
 }
-SquareTable(n);
+
 void SquareTable(int num)
 {
     for (int i = 1; i <= num;i++)
     {
         int square = i*i;
-        Console.WriteLine($"{i, 3}  {square, 5}");
+        Console.WriteLine($"{i, 1}  {square, 3}");
     }
 }
+
+SquareTable(n);
