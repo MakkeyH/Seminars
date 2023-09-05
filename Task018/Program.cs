d5f033fee46aad0e5f1f2aa11da444161b61e52f
MakarovaElena <lena-chester@mai.ru>
2023-09-05 21:24:50 +0300

    Диапазон координат X и Y по номеру заданной четверти Task018

--- Task018/Program.cs
+++ Task018/Program.cs
@@ -2,20 +2,36 @@
 // заданному номеру четверти, показывает диапазон
 // возможных координат точек в этой четверти (x и y).
 
+// Console.WriteLine("Введите номер четверти: ");
+// string quarter = Console.ReadLine();
+
+// string coordinates = Range(quarter);
+
+// if (coordinates != null)
+//     Console.WriteLine(coordinates);
+// else Console.WriteLine("Некорректная четверть");
+
+// string Range(string q)
+// {
+//     if (q == "1") return "x > 0 && y > 0";
+//     if (q == "2") return "x < 0 && y > 0";
+//     if (q == "3") return "x < 0 && y < 0";
+//     if (q == "4") return "x > 0 && y < 0";
+//     return null;
+// }
+
+
 Console.WriteLine("Введите номер четверти: ");
-string quarter = Console.ReadLine();
+int quarter = Convert.ToInt32(Console.ReadLine());
 
-string coordinates = Range(quarter);
-
-if (coordinates != null)
-    Console.WriteLine(coordinates);
-else Console.WriteLine("Некорректная четверть");
-
-string Range(string q)
+void Range(int q)
 {
-    if (q == "1") return "x > 0 && y > 0";
-    if (q == "2") return "x < 0 && y > 0";
-    if (q == "3") return "x < 0 && y < 0";
-    if (q == "4") return "x > 0 && y < 0";
-    return null;
+    if (q == 1)  Console.WriteLine("x > 0 && y > 0");
+    else if (q == 2)  Console.WriteLine("x < 0 && y > 0");
+    else if (q == 3)  Console.WriteLine("x < 0 && y < 0");
+    else if (q == 4)  Console.WriteLine("x > 0 && y < 0");
+    else Console.WriteLine("Некорректная четверть");
 }
+
+Range(quarter);
+
