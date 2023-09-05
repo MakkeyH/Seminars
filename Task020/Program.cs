47ed865e9e1f383cf3ecefa6e3716ff52e8f4f20
MakarovaElena <lena-chester@mai.ru>
2023-09-05 21:54:17 +0300

    Расстояние между двумя точками с заданными координатами в 2D пространстве Task020

--- Task020/Program.cs
+++ Task020/Program.cs
@@ -20,25 +20,37 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
-Console.WriteLine("Введите координаты первой точки");
-Console.Write("X1: ");
+// Console.WriteLine("Введите координаты первой точки");
+// Console.Write("X1: ");
+// int a1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y1: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координаты второй точки");
+// Console.Write("X2: ");
+// int a2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y2: ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+
+// double dist = Distance(a1, b1, a2, b2);
+// double distRound = Math.Round(dist, 2, MidpointRounding.ToZero);
+// double Distance(int x1, int y1, int x2, int y2)
+// {
+//     double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+//     return distance;
+// }
+// Console.WriteLine($"Расстояние между координатами A и B - {distRound}");
+
+
+Console.WriteLine("Введите координаты точки A:");
 int a1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y1: ");
 int b1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координаты второй точки");
-Console.Write("X2: ");
+Console.WriteLine("Введите координаты точки B: ");
 int a2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y2: ");
 int b2 = Convert.ToInt32(Console.ReadLine());
 
-double dist = Distance(a1, b1, a2, b2);
-double distRound = Math.Round(dist, 2, MidpointRounding.ToZero);
-double Distance(int x1, int y1, int x2, int y2)
-{
-    double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    return distance;
-}
-Console.WriteLine($"Расстояние между координатами A и B - {distRound}");
-
-
+double length;
+length = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+double distRound = Math.Round(length, 2, MidpointRounding.ToZero);
+Console.Write(distRound);
