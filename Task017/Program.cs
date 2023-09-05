ce648f69b6d4655e5737081a54c5cf6245d8c466
MakarovaElena <lena-chester@mai.ru>
2023-09-05 21:05:32 +0300

    Четверти через координаты X и Y Task017

--- Task017/Program.cs
+++ Task017/Program.cs
@@ -3,24 +3,42 @@
 // номер четверти плоскости, в которой находится эта
 // точка.
 
+// Console.WriteLine("Введите координаты точки");
+// Console.Write("X: ");
+// int xCoordinate = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+// int quarter = Quarter(xCoordinate, yCoordinate);
+// string result = quarter > 0
+//                 ? $"Указанные координаты соответствуют четверти {quarter}"
+//                 : "Введены некорректные координаты";
+
+// Console.WriteLine(result);
+
+// int Quarter(int x, int y)
+// {
+//     if (x > 0 && y > 0) return 1;
+//     if (x < 0 && y > 0) return 2;
+//     if (x < 0 && y < 0) return 3;
+//     if (x > 0 && y < 0) return 4;
+//     return 0;
+// }
+
+
 Console.WriteLine("Введите координаты точки");
 Console.Write("X: ");
 int xCoordinate = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int yCoordinate = Convert.ToInt32(Console.ReadLine());
 
-int quarter = Quarter(xCoordinate, yCoordinate);
-string result = quarter > 0
-                ? $"Указанные координаты соответствуют четверти {quarter}"
-                : "Введены некорректные координаты";
-
-Console.WriteLine(result);
-
-int Quarter(int x, int y)
+void Quarter(int x, int y)
 {
-    if (x > 0 && y > 0) return 1;
-    if (x < 0 && y > 0) return 2;
-    if (x < 0 && y < 0) return 3;
-    if (x > 0 && y < 0) return 4;
-    return 0;
+    if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+    else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+    else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+    else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+    else Console.WriteLine("Точка находится на координатной прямой");
 }
+
+Quarter(xCoordinate, yCoordinate);
