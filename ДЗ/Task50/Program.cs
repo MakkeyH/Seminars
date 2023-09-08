8cd85b5b2fc389683cc376844ea3b939d5d466f8
MakarovaElena <lena-chester@mai.ru>
2023-09-08 19:49:57 +0300

    Двумерный рандомный массив размером mxn,позиции элемента в массиве с выводом значения в массиве,либо указанием отсутствия такого элемента Task50

---
+++ ДЗ/Task50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого элемента в массиве нет
+
+
+Console.WriteLine("Введите номер строки в двумерном массиве: ");
+int a = Convert.ToInt32(Console.ReadLine()); // первая позиция элемента будет строкой;
+
+Console.WriteLine("Введите номер столбца в двумерном массиве: ");
+int b = Convert.ToInt32(Console.ReadLine()); // вторая позиция элемента будет столбцом;
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool ElementValue(int[,] matrix, int i, int j)
+{
+    if (i < matrix.GetLength(0) && j < matrix.GetLength(1)) return true;
+    else return false;
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+bool result = ElementValue(array2d, a, b);
+Console.WriteLine($"{result} ? {array2d[a, b]} : Такого элемента не существует");
+
