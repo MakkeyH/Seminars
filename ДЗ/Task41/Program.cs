2202d95e000e3b54ec4d0579bdb581120f06d8c9
MakarovaElena <lena-chester@mai.ru>
2023-09-08 21:33:33 +0300

    Количество чисел,введённое пользователем, больше 0 Task41

---
+++ ДЗ/Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine ("- Сколько чисел вы хотите ввести?");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[count];
+
+void ArrayNumbers()
+{
+    Console.WriteLine("Введите ваши числа: ");
+    for (int i = 0; i < count; i++)
+    {
+        if (i > 0)
+        {
+            Console.Write("Ещё: ");
+        }
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int CountNum()
+{
+    int count1 = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count1++;
+        }
+    }
+    return count1;
+}
+
+ArrayNumbers();
+Console.WriteLine($"Вами введено чисел больше 0: {CountNum()}");
+
