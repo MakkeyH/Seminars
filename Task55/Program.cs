3156806b5d3f230df1bd6cb075005d5af750755d
MakarovaElena <lena-chester@mai.ru>
2023-09-12 13:55:27 +0300

    Двумерный рандомный массив c заменой строк на столбцы,либо невозможностью данного действия Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapRowsColumns(int[,] matrix)
+{
+    // for (int i = 1; i < matrix.GetLength(0); i++) // 1-0   2-0   2-1   3-0   3-1   3-2
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++) // 0-1   0-2   0-3   1-2   1-3   2-3
+    {
+        // for (int j = 0; j < i; j++)
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+bool ValidateSquareMatrix(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 3, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+if (ValidateSquareMatrix(array2d))
+{
+    SwapRowsColumns(array2d);
+    PrintMatrix(array2d);
+}
+else Console.WriteLine("Невозможно заменить строки на столбцы.");
